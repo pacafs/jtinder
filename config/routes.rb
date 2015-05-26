10cdb2b64210e76826b1d0a4712853d43d45526d
Pedro Fernandes da Silva <paca.fs@gmail.com>
2015-05-26 17:37:49 +0100

    App Finished

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   
   match 'sign_out', to: 'sessions#destroy', via: :delete
 
+  get 'matches/get_email' => 'matches#get_email'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
