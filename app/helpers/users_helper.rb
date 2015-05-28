3ba38c6909c1dab5d6477a089481486d4fa3bf8b
Pedro Fernandes da Silva <paca.fs@gmail.com>
2015-05-28 11:23:07 +0100

    user_helper updated

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,35 +1,40 @@
 module UsersHelper
 
 	def name(user)
-		case user
-			when user
-				user.name.partition(' ').first
-			when @user
-				@user.name.partition(' ').first
+		if user.name
+			case user
+				when user
+					user.name.partition(' ').first
+				when @user
+					@user.name.partition(' ').first
+			end
 		end
 	end
 
 	def age(user)
-		case user
-			when user
-				age = Date.today.year - user.date_of_birth.year 
-	        	age -= 1 if Date.today < user.date_of_birth + age.years
-	        	return age
-			when @user
-				age = Date.today.year - @user.date_of_birth.year 
-	        	age -= 1 if Date.today < @user.date_of_birth + age.years
-	        	return age
+		if user.date_of_birth
+			case user
+				when user
+					age = Date.today.year - user.date_of_birth.year 
+		        	age -= 1 if Date.today < user.date_of_birth + age.years
+		        	return age
+				when @user
+					age = Date.today.year - @user.date_of_birth.year 
+		        	age -= 1 if Date.today < @user.date_of_birth + age.years
+		        	return age
+			end
 		end
 	end
 
 	def location(user)
-		case user
-			when user
-				user.location.partition(',').first
-			when @user
-				@user.location.partition(',').first
+		if user.location
+			case user
+				when user
+					user.location.partition(',').first
+				when @user
+					@user.location.partition(',').first
+			end
 		end
 	end
-
 
 end
